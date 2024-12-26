f5343ea93cdca07aabf4c02535d7ed6d35e4b6d3
Sonny010 <302880842@qq.com>
2024-12-26 15:03:26 +0800

    add douban.py

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ import json
 url = "https://movie.douban.com/j/search_subjects?type=movie&tag=%E6%9C%80%E6%96%B0&page_limit=50&page_start=0"
 header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"}
 html = requests.get(url, headers=header)
-#tml.text)print(h
-json_html = json.loads(html.text)
+
+json_html = json.loads(html.text)#json转字典
+
 movies = json_html["subjects"][0]
-rate =  movies["rate"]
+rate = movies["rate"]
 title = movies["title"]
 print("分数：" +str(rate) + ", 名字：" + str(title))
